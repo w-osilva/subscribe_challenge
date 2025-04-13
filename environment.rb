49363ea2f2dbdb8a005a62a4975f56e7797b1989
Washington Silva <washington.silva.dev@gmail.com>
2025-04-12 21:20:07 -0300

    chore: Add environment configuration with Zeitwerk loader

---
+++ environment.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'zeitwerk'
+loader = Zeitwerk::Loader.new
+# load libraries
+loader.push_dir('lib')
+loader.collapse('lib/cli')
+loader.collapse('lib/models')
+loader.setup
