1f3501d1ef7a099a483c3a4fd5100ca8faf2cd35
Washington Silva <washington.silva.dev@gmail.com>
2025-04-24 14:48:42 -0300

    chore: Remove rack-test gem and related require statement

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@
 
 source 'https://rubygems.org'
 
-gem 'rack-test'
 gem 'rspec'
 gem 'rubocop'
 gem 'zeitwerk'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../environment'
-require 'rack/test'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
